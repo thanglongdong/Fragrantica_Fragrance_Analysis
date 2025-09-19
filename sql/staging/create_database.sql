13109ad682b5d406c19186704a23ba6ff9e4cdb4
thanglongdong <thanglongdong@gmail.com>
2025-09-19 22:45:47 +1000

    Modify schemas for data import (staging)

--- sql/staging/create_database.sql
+++ sql/staging/create_database.sql
@@ -1,3 +1,4 @@
+-- Create the database if it doesn't exist
 CREATE DATABASE fragrantica_frag;
 
-DROP DATABASE IF EXISTS fragrantica_frag;
+-- DROP DATABASE IF EXISTS fragrantica_frag;
--- sql/staging/staging_tables.sql
+++ sql/staging/staging_tables.sql
@@ -23,17 +23,6 @@ CREATE TABLE IF NOT EXISTS public.staging_fragrance_core (
     url TEXT
 )
 
--- Create fragrance core staging table for data import
-CREATE TABLE IF NOT EXISTS public.staging_fragrance_list (
-    fragrance_name VARCHAR(255),
-    gender VARCHAR(50),
-    rating_value NUMERIC(3,2),
-    rating_count INT,
-    main_notes TEXT,
-    description TEXT,
-    url TEXT
-)
-
 -- Load data into the tables using COPY command
 COPY staging_brand
 FROM 'C:\Users\thang\OneDrive\Documents\Fragrantica_Fragrance_Analysis\data\staging\brand_excelcleaned.csv'
@@ -43,6 +32,22 @@ COPY staging_fragrance_core
 FROM 'C:\Users\thang\OneDrive\Documents\Fragrantica_Fragrance_Analysis\data\staging\fragrance_core.csv'
 WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
 
-COPY staging_fragrance_list
-FROM 'C:\Users\thang\OneDrive\Documents\Fragrantica_Fragrance_Analysis\data\staging\fragrance_list.csv'
-WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+-- Verify data load by retrieving 10 rows from each table
+SELECT * FROM staging_brand LIMIT 10;
+SELECT * FROM staging_fragrance_core LIMIT 10;
+
+/* Instructions to load data into staging tables using pgAdmin or when "Permission denied" Error occurs
+
+1. Open pgAdmin
+2. In Object Explorer (left-hand pane), navigate to `sql_course` database
+3. Right-click `sql_course` and select `PSQL Tool`
+            - This opens a terminal window to write the following code
+4. Get the absolute file path of your csv files
+            - Find path by right-clicking a CSV file in VS Code and selecting “Copy Path”
+5. Paste the following into `PSQL Tool`,
+
+\COPY staging_brand FROM 'C:/Users/thang/OneDrive/Documents/Fragrantica_Fragrance_Analysis/data/staging\brand_excelcleaned.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+
+\COPY staging_fragrance_core FROM 'C:/Users/thang/OneDrive/Documents/Fragrantica_Fragrance_Analysis/data/staging/fragrance_core.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+
+*/
