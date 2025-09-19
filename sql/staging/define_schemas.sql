db767e6a92de70f46a02d2abd6818933a8a7b808
thanglongdong <thanglongdong@gmail.com>
2025-09-19 23:08:17 +1000

    Modify schemas for final tables (staging)

---
+++ sql/staging/define_schemas.sql
@@ -0,0 +1,53 @@
+-- Define the schemas for the final tables
+
+-- Create brand table
+CREATE TABLE brand (
+    brand_id INT PRIMARY KEY,
+    brand_name VARCHAR(255) NOT NULL,
+    country VARCHAR(255)
+)
+
+-- Create fragrance list table
+CREATE TABLE fragrance_list (
+    fragrance_id SERIAL INT PRIMARY KEY,
+    fragrance_name VARCHAR(255) NOT NULL,
+    brand_id INT REFERENCES brand(brand_id),
+    gender VARCHAR(50),
+    rating_value NUMERIC(3,2),
+    rating_count INT,
+    year INT,
+    url TEXT,
+)
+
+-- Create notes table and junction table for many-to-many relationship
+CREATE TABLE note (
+    note_id SERIAL INT PRIMARY KEY,
+    note_name VARCHAR(255) NOT NULL
+)
+
+CREATE TABLE fragrance_note (
+    fragrance_id INT REFERENCES fragrance_list(fragrance_id),
+    note_id INT REFERENCES note(note_id),
+    note_type VARCHAR(50)
+)
+-- Create perfumer table and junction table for many-to-many relationship
+CREATE TABLE perfumer (
+    perfumer_id SERIAL INT PRIMARY KEY,
+    perfumer_name VARCHAR(555) NOT NULL
+)
+
+CREATE TABLE fragrance_perfumer (
+    fragrance_id INT REFERENCES fragrance_list(fragrance_id),
+    perfumer_id INT REFERENCES perfumer(perfumer_id)
+)
+
+-- Create accord table and junction table for many-to-many relationship
+CREATE TABLE accord (
+    accord_id SERIAL INT PRIMARY KEY,
+    accord_name VARCHAR(255) NOT NULL
+)
+
+CREATE TABLE fragrance_accord (
+    fragrance_id INT REFERENCES fragrance_list(fragrance_id),
+    accord_name VARCHAR(255) NOT NULL
+)
