6259290571b6dc50f659c50021a0f6a5058af524
thanglongdong <thanglongdong@gmail.com>
2025-09-24 16:57:46 +1000

    Correct error for schema definition (staging)

--- sql/staging/define_schemas.sql
+++ sql/staging/define_schemas.sql
@@ -9,19 +9,19 @@ CREATE TABLE brand (
 
 -- Create fragrance list table
 CREATE TABLE fragrance_list (
-    fragrance_id SERIAL INT PRIMARY KEY,
+    fragrance_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     fragrance_name VARCHAR(255) NOT NULL,
     brand_id INT REFERENCES brand(brand_id),
     gender VARCHAR(50),
     rating_value NUMERIC(3,2),
     rating_count INT,
     year INT,
-    url TEXT,
+    url TEXT
 )
 
 -- Create notes table and junction table for many-to-many relationship
 CREATE TABLE note (
-    note_id SERIAL INT PRIMARY KEY,
+    note_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     note_name VARCHAR(255) NOT NULL
 )
 
@@ -32,7 +32,7 @@ CREATE TABLE fragrance_note (
 )
 -- Create perfumer table and junction table for many-to-many relationship
 CREATE TABLE perfumer (
-    perfumer_id SERIAL INT PRIMARY KEY,
+    perfumer_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     perfumer_name VARCHAR(555) NOT NULL
 )
 
@@ -43,7 +43,7 @@ CREATE TABLE fragrance_perfumer (
 
 -- Create accord table and junction table for many-to-many relationship
 CREATE TABLE accord (
-    accord_id SERIAL INT PRIMARY KEY,
+    accord_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     accord_name VARCHAR(255) NOT NULL
 )
 
@@ -51,3 +51,10 @@ CREATE TABLE fragrance_accord (
     fragrance_id INT REFERENCES fragrance_list(fragrance_id),
     accord_name VARCHAR(255) NOT NULL
 )
+
+-- Verify the tables have been created
+SELECT table_schema, table_name
+FROM information_schema.tables
+WHERE table_type = 'BASE TABLE'  -- ensure to list actual tables (not views or foreign tables)
+  AND table_schema NOT IN ('pg_catalog', 'information_schema') -- exclude system schemas
+ORDER BY table_schema, table_name;
